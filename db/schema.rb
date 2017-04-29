4f6df2a709c7b5dff3b3e23746f098fb11acc0a3
Jesus Miramontes <jesus_miramontes@me.com>
2017-04-29 16:27:25 -0500

    Model Article created

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170429212216) do
+
+  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "body",         limit: 65535
+    t.integer  "visits_count"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
