e8a9e7bacc80ce6638ce6f09267cb340dc9967d1
Jesus Miramontes <jesus_miramontes@me.com>
2017-05-04 01:26:38 -0500

    Relation user:has_many articles ; article:belongs_to user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, uniqueness: true #Valida que no esté vacio, y no esté repetido
   validates :body, presence: true, length: {minimum: 10} #Valida tamaño minimo
 end
