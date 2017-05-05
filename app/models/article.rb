e37db93576b3e9312b08778dcd5cb12243474e3d
Jesus Miramontes <jesus_miramontes@me.com>
2017-05-05 17:36:37 -0500

    issue fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, uniqueness: true #Valida que no esté vacio, y no esté repetido
   validates :body, presence: true, length: {minimum: 10} #Valida tamaño minimo
-  before_save :set_visits_count
+  before_create :set_visits_count
 
   private
   def set_visits_count
