d294257a3e04952d3eb6892059355a74ef63d7c3
Jesus Miramontes <jesus_miramontes@me.com>
2017-05-05 17:32:03 -0500

    Before save an article set visits count to 0

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, uniqueness: true #Valida que no esté vacio, y no esté repetido
   validates :body, presence: true, length: {minimum: 10} #Valida tamaño minimo
+  before_save :set_visits_count
+
+  private
+  def set_visits_count
+    self.visits_count=0;
+  end
 end
