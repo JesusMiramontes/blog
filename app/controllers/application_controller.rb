edc6c7ab865db0af7b8b0b5a282d09612792e38c
Jesus Miramontes <jesus_miramontes@me.com>
2017-05-14 03:29:02 -0500

    Show categories on top nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_category
+
+  private
+  def set_category
+    @categories = Category.all
+  end
 end
