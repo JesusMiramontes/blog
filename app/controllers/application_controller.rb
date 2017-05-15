509976df7561691c251c6bd9c9e68b90a9c7b708
Jesus Miramontes <jesus_miramontes@me.com>
2017-05-15 15:59:44 -0500

    Verify if user has the requiered level before do any action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_category
 
+  protected
+  def authenticate_editor!
+    redirect_to root_path, notice: "You don't have the required permission level" unless user_signed_in? && current_user.is_editor?
+  end
+
+  def authenticate_admin!
+    redirect_to root_path, notice: "You don't have the required permission level" unless user_signed_in? && current_user.is_admin?
+  end
   private
   def set_category
     @categories = Category.all
